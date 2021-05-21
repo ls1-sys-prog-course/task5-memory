0a4813dc4736bfcb09bba4e0359acbacd004581e
dimstav23 <dimstav23@gmail.com>
2021-05-21 19:39:22 +0200

    malloc larson bench test added - not included in the automated tests yet

--- tests/threadtest.py
+++ tests/threadtest.py
@@ -9,12 +9,14 @@ from testsupport import run, run_project_executable, subtest, test_root, warn, e
 def main() -> None:
     # Get test abspath
     min_scaling_factor = 1.2
-    testname = "threadtest"
+    testname = ["threadtest"]#, "larson"]
     lib = ensure_library("libmymalloc.so")
     with tempfile.TemporaryDirectory() as tmpdir:
-        test = test_root().joinpath(testname)
-        if not test.exists():
-            run(["make", "-C", str(test_root()), str(test)])
+
+        for test in testname:
+            test_file = test_root().joinpath(test)
+            if not test_file.exists():
+                run(["make", "-C", str(test_root()), str(test)])
         
         # Check that libmymalloc.so provides malloc,free,calloc,realloc symbols
         with subtest("Check that malloc,realloc,free,calloc functions are available in libmymalloc.so"):
@@ -36,29 +38,42 @@ def main() -> None:
                     exit(1)
         
         num_of_threads = [1, 2, 4]
-        exec_time = []
-        for threads in num_of_threads:
-            with subtest(f"Run {testname} with {lib} preloaded with {threads} threads"):
-                with open(f"{tmpdir}/stdout", "w+"
-                    ) as stdout:
-                    run_project_executable(
-                            str(test),
-                            args=[str(threads)],
-                            stdout=stdout,
-                            extra_env={"LD_PRELOAD": str(lib)}
-                            )
-                    if os.stat(f"{tmpdir}/stdout").st_size > 0:
-                        exec_time.append(float(open(f"{tmpdir}/stdout").readlines()[0].strip()))
-                    if os.stat(f"{tmpdir}/stdout").st_size == 0:
-                        warn("Test failed with error")
-                        exit(1)
-        
-        s1_2 = exec_time[0] / exec_time[1]
-        s2_4 = exec_time[1] / exec_time[2]
+        res = []
 
-        if (s1_2 < min_scaling_factor or s1_2 > 2 or s2_4 < min_scaling_factor or s2_4 > 2):
-            warn("Allocator does not scale properly. Execution times : " + str(exec_time))
-            exit(1)
+        for test in testname:
+            test_file = test_root().joinpath(test)
+            for threads in num_of_threads:
+                with subtest(f"Run {test} with {lib} preloaded with {threads} thread(s)"):
+                    with open(f"{tmpdir}/stdout", "w+"
+                        ) as stdout:
+                        run_project_executable(
+                                str(test_file),
+                                args=[str(threads)],
+                                stdout=stdout,
+                                extra_env={"LD_PRELOAD": str(lib)}
+                                )
+                        if os.stat(f"{tmpdir}/stdout").st_size > 0:
+                            res.append(float(open(f"{tmpdir}/stdout").readlines()[0].strip()))
+                        if os.stat(f"{tmpdir}/stdout").st_size == 0:
+                            warn("Test failed with error")
+                            exit(1)
+            
+            if (test=="threadtest"):
+                s1_2 = res[0] / res[1]
+                s2_4 = res[1] / res[2]
+                if (s1_2 < min_scaling_factor or s2_4 < min_scaling_factor):
+                    warn("Allocator does not scale properly. Execution times : " + str(res))
+                    exit(1)
+                else:
+                    res = []
+            elif (test=="larson"):
+                s1_2 = res[1] / res[0]
+                s2_4 = res[2] / res[1]
+                if (s1_2 < min_scaling_factor or s2_4 < min_scaling_factor):
+                    warn("Allocator does not scale properly. Throughput : " + str(res))
+                    exit(1)
+                else:
+                    res = []
         
 if __name__ == "__main__":
     main()
